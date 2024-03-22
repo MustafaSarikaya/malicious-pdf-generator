8e5c6795e9cb61d15615ec797c4dc152962abd35
jeremie1207 <jbarh086@uottawa.ca>
2024-03-22 12:10:38 -0400

    add comments to check_open_action

--- malicious_pdf_generator/check_open_action.py
+++ malicious_pdf_generator/check_open_action.py
@@ -1,9 +1,20 @@
-from pdfalyzer.decorators.pdf_tree_node import PdfTreeNode
 from pdfalyzer.pdfalyzer import Pdfalyzer
 from anytree.search import findall_by_attr
 
 
 def check_open_action(pdf_path: str) -> bool:
+    """
+    Checks if a given PDF file has an OpenAction that is a JavaScript action.
+
+    Parameters:
+        pdf_path (str): The path to the PDF file.
+
+    Returns:
+        bool: True if the OpenAction is a JavaScript action, False otherwise.
+
+    Raises:
+        Exception: If an error occurs while parsing the PDF's object tree.
+    """
     pdfalyzer = Pdfalyzer(pdf_path)
     try:
         page_nodes = findall_by_attr(pdfalyzer.pdf_tree, name='type', value='/Catalog')
