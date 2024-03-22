b17fdc5cc39408fab3c749671e31249df88acb8c
jeremie1207 <jbarh086@uottawa.ca>
2024-03-22 12:04:43 -0400

    update main.py file

--- main.py
+++ main.py
@@ -5,4 +5,3 @@ if __name__ == "__main__":
     obfuscated_code = obfuscated_code(const.JS_CODE)
     embedded_javascript_into_pdf(obfuscated_code, const.input_pdf, const.output_pdf)
     print(f"JavaScript code embedded into {const.output_pdf}.")
-    check_open_action(const.output_pdf)
